0dab9cac34ea20f8101f7203fa48478275e36323
Andrea (EtabetaWeb) Cervi <andrea.cervi@poste.it>
2018-11-12 17:42:31 +0100

    release 1.0.x

---
+++ FatturaEL.v13/FatturaElettronica.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Xml.Serialization
+Imports System.Xml
+
+<Serializable>
+Public Class FatturaElettronica
+    <XmlAttribute("versione")>
+    Public versione As String
+    Public FatturaElettronicaHeader As FatturaElettronicaHeader
+    Public FatturaElettronicaBody As FatturaElettronicaBody
+End Class
